199140a21052fa225811ec389a53f1c5bda12505
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-11 19:30:23 +0900

    chore: 컴포넌트 몇 개만 불러오도록 수정

--- multipleEntryImports/apps/wow-docs/app/page.tsx
+++ multipleEntryImports/apps/wow-docs/app/page.tsx
@@ -10,46 +10,6 @@ import TextField7 from "multiple-entry-imports-ui/TextField7";
 import TextField8 from "multiple-entry-imports-ui/TextField8";
 import TextField9 from "multiple-entry-imports-ui/TextField9";
 import TextField10 from "multiple-entry-imports-ui/TextField10";
-import TextField11 from "multiple-entry-imports-ui/TextField11";
-import TextField12 from "multiple-entry-imports-ui/TextField12";
-import TextField13 from "multiple-entry-imports-ui/TextField13";
-import TextField14 from "multiple-entry-imports-ui/TextField14";
-import TextField15 from "multiple-entry-imports-ui/TextField15";
-import TextField16 from "multiple-entry-imports-ui/TextField16";
-import TextField17 from "multiple-entry-imports-ui/TextField17";
-import TextField18 from "multiple-entry-imports-ui/TextField18";
-import TextField19 from "multiple-entry-imports-ui/TextField19";
-import TextField20 from "multiple-entry-imports-ui/TextField20";
-import TextField21 from "multiple-entry-imports-ui/TextField21";
-import TextField22 from "multiple-entry-imports-ui/TextField22";
-import TextField23 from "multiple-entry-imports-ui/TextField23";
-import TextField24 from "multiple-entry-imports-ui/TextField24";
-import TextField25 from "multiple-entry-imports-ui/TextField25";
-import TextField26 from "multiple-entry-imports-ui/TextField26";
-import TextField27 from "multiple-entry-imports-ui/TextField27";
-import TextField28 from "multiple-entry-imports-ui/TextField28";
-import TextField29 from "multiple-entry-imports-ui/TextField29";
-import TextField30 from "multiple-entry-imports-ui/TextField30";
-import TextField31 from "multiple-entry-imports-ui/TextField31";
-import TextField32 from "multiple-entry-imports-ui/TextField32";
-import TextField33 from "multiple-entry-imports-ui/TextField33";
-import TextField34 from "multiple-entry-imports-ui/TextField34";
-import TextField35 from "multiple-entry-imports-ui/TextField35";
-import TextField36 from "multiple-entry-imports-ui/TextField36";
-import TextField37 from "multiple-entry-imports-ui/TextField37";
-import TextField38 from "multiple-entry-imports-ui/TextField38";
-import TextField39 from "multiple-entry-imports-ui/TextField39";
-import TextField40 from "multiple-entry-imports-ui/TextField40";
-import TextField41 from "multiple-entry-imports-ui/TextField41";
-import TextField42 from "multiple-entry-imports-ui/TextField42";
-import TextField43 from "multiple-entry-imports-ui/TextField43";
-import TextField44 from "multiple-entry-imports-ui/TextField44";
-import TextField45 from "multiple-entry-imports-ui/TextField45";
-import TextField46 from "multiple-entry-imports-ui/TextField46";
-import TextField47 from "multiple-entry-imports-ui/TextField47";
-import TextField48 from "multiple-entry-imports-ui/TextField48";
-import TextField49 from "multiple-entry-imports-ui/TextField49";
-import TextField50 from "multiple-entry-imports-ui/TextField50";
 
 const Home = () => {
   return (
@@ -64,46 +24,6 @@ const Home = () => {
       <TextField8 label="TextField8" />
       <TextField9 label="TextField9" />
       <TextField10 label="TextField10" />
-      <TextField11 label="TextField11" />
-      <TextField12 label="TextField12" />
-      <TextField13 label="TextField13" />
-      <TextField14 label="TextField14" />
-      <TextField15 label="TextField15" />
-      <TextField16 label="TextField16" />
-      <TextField17 label="TextField17" />
-      <TextField18 label="TextField18" />
-      <TextField19 label="TextField19" />
-      <TextField20 label="TextField20" />
-      <TextField21 label="TextField21" />
-      <TextField22 label="TextField22" />
-      <TextField23 label="TextField23" />
-      <TextField24 label="TextField24" />
-      <TextField25 label="TextField25" />
-      <TextField26 label="TextField26" />
-      <TextField27 label="TextField27" />
-      <TextField28 label="TextField28" />
-      <TextField29 label="TextField29" />
-      <TextField30 label="TextField30" />
-      <TextField31 label="TextField31" />
-      <TextField32 label="TextField32" />
-      <TextField33 label="TextField33" />
-      <TextField34 label="TextField34" />
-      <TextField35 label="TextField35" />
-      <TextField36 label="TextField36" />
-      <TextField37 label="TextField37" />
-      <TextField38 label="TextField38" />
-      <TextField39 label="TextField39" />
-      <TextField40 label="TextField40" />
-      <TextField41 label="TextField41" />
-      <TextField42 label="TextField42" />
-      <TextField43 label="TextField43" />
-      <TextField44 label="TextField44" />
-      <TextField45 label="TextField45" />
-      <TextField46 label="TextField46" />
-      <TextField47 label="TextField47" />
-      <TextField48 label="TextField48" />
-      <TextField49 label="TextField49" />
-      <TextField50 label="TextField50" />
     </>
   );
 };

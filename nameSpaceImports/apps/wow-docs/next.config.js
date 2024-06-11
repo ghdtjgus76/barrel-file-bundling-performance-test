3e425022babd6d3d5c755a4137fc6c2e4b2d56ba
ghdtjgus76 <ghdtjgus76@naver.com>
2024-06-11 15:05:04 +0900

    feat: nameSpaceImports 폴더 세팅

---
+++ nameSpaceImports/apps/wow-docs/next.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+/** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: true,
+  openAnalyzer: true,
+});
+
+module.exports = withBundleAnalyzer({
+  transpilePackages: ["wowds-ui"],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+});
